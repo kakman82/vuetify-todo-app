b3fe58f38a2d12c72b21a57d9a42b4a3ad5dcd2b
kakman82 <akmankerem@gmail.com>
2021-01-29 22:09:23 +0300

    target: static added to nuxt.config for netlify deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+
+  // Netlify deploy için gerekliymiş ref: https://nuxtjs.org/docs/2.x/deployment/netlify-deployment
+  target: 'static',
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     // titleTemplate: '%s - vuetify-todo-udemy',
